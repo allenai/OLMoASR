c9f96afa450229e2d412e5b47dcf238f5300ada0
huongngo-8 <hvn2002@uw.edu>
2024-01-20 00:50:39 +0000

    commit on main

---
+++ src/audio.py
@@ -0,0 +1,36 @@
+# break audio files into 30-second segments paired with subset of transcript that occurs within that time segment
+# audio is resampled to 16000 hz, 80-channel log-magnitude mel spectrogram, computed with a 25ms window and 10ms hop size
+# spectrogram is a visual representation of spectrum of frequencies of a signal as it varies with time
+# mel spectrogram is a spectrogram that is generated using the Mel scale, a perceptual scale of pitches judged by listeners to be equal in distance from one another
+# 80-channel mel spectrogram is a mel spectrogram represented by 80 mel bins (frequency channels)
+# feature normalization: globally scale input to be between [-1, 1] with approximate mean 0 across pre-training dataset
+import yt_dlp
+import os
+import subprocess
+import webvtt
+
+def download_audio_transcript(video_id, lang_code, output_dir):
+    if lang_code == "unknown":
+        lang_code = "en"
+
+    command = [
+        "yt-dlp",
+        "--write-subs",
+        "--no-write-auto-subs",
+        "--skip-download",
+        "--sub-format",
+        "vtt",
+        "--sub-langs",
+        f"{lang_code},-live_chat",
+        f"https://www.youtube.com/watch?v={video_id}",
+        "-o",
+        f"{output_dir}/%(id)s",
+    ]
+    subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def read_vtt(file_path):
+    vtt = webvtt.read(file_path)
+    text = []
+    for caption in vtt:
+        text.append(caption.text)
+    return text
