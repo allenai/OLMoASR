44196849fee4b688bf45ee76956d469c1ed2dff7
huongngo-8 <hvn2002@uw.edu>
2024-01-21 00:24:14 +0000

    commit on main

--- open_whisper/audio.py
+++ open_whisper/audio.py
@@ -10,24 +10,33 @@ import subprocess
 import webvtt
 
 
-def download_audio_transcript(video_id, lang_code, output_dir):
+def download_transcript(video_id, lang_code, output_dir):
     if lang_code == "unknown":
         lang_code = "en"
 
     command = [
         "yt-dlp",
-        "-f",
-        "bestaudio",
         "--write-subs",
         "--no-write-auto-subs",
-        # "--skip-download",
+        "--skip-download",
         "--sub-format",
         "vtt",
         "--sub-langs",
         f"{lang_code},-live_chat",
         f"https://www.youtube.com/watch?v={video_id}",
         "-o",
-        f"{output_dir}/%(id)s",
+        f"{output_dir}/%(id)s/%(id)s.%(ext)s",
+    ]
+    subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def download_audio(video_id, output_dir):
+    command = [
+        "yt-dlp",
+        "-f",
+        "bestaudio[asr=16000][ext=wav]",
+        f"https://www.youtube.com/watch?v={video_id}",
+        "-o",
+        f"{output_dir}/%(id)s/%(id)s.%(ext)s",
     ]
     subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
