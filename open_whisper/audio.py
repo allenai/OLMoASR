fa46ffa809054ea2f08a0bc9afb0477176ff7d38
huongngo-8 <hvn2002@uw.edu>
2024-01-23 22:41:08 +0000

    commit on main

--- open_whisper/audio.py
+++ open_whisper/audio.py
@@ -90,22 +90,22 @@ def read_vtt(file_path: str) -> Tuple[Dict, str, str]:
 
 
 def trim_audio(
-    audio_file: str, start: str, end: str, window: int, output_dir: str
+    audio_file: str, start: str, end: str, start_window: int, end_window: int, output_dir: str
 ) -> None:
-    start = adjust_timestamp(start, -window)
-    end = adjust_timestamp(end, window)
+    adjusted_start = adjust_timestamp(start, start_window)
+    adjusted_end = adjust_timestamp(end, end_window)
 
     command = [
         "ffmpeg",
         "-i",
         audio_file,
         "-ss",
-        start,
+        adjusted_start,
         "-to",
-        end,
+        adjusted_end,
         "-c",
         "copy",
-        f"{output_dir}/{audio_file.split('/')[-2]}_trimmed.{audio_file.split('.')[-1]}",
+        f"{output_dir}/{start}_{end}.{audio_file.split('.')[-1]}",
     ]
 
     subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -141,117 +141,181 @@ def chunk_audio(audio_file: str, output_dir: str, transcript_start: str) -> None
         transcript_start = new_time
 
 
-def chunk_transcript(transcript_file: str, output_dir: str) -> None:
-    output_dir = output_dir + "/segments"
-    os.makedirs(output_dir, exist_ok=True)
+def chunk_audio_transcript(
+    transcript_file: str, audio_file: str, t_output_dir: str, a_output_dir: str
+):
+    t_output_dir = t_output_dir + "/segments"
+    a_output_dir = a_output_dir + "/segments"
+    os.makedirs(t_output_dir, exist_ok=True)
+    os.makedirs(a_output_dir, exist_ok=True)
 
     transcript, *_ = read_vtt(transcript_file)
     a = 0
     b = 0
+
     timestamps = list(transcript.keys())
-    start = timestamps[a][0]
-    # end = timestamps[b][1]
-    # text = transcript[(start, end)]
+    diff = 0
     init_diff = 0
     text = ""
-    added_silence = False
-    remain_text = ""
 
     while a < len(transcript) + 1:
+        init_diff = calculate_difference(timestamps[a][0], timestamps[b][1])
         if init_diff < 30000:
-            if init_diff == 0 and convert_to_milliseconds(
-                adjust_timestamp(start, 30)
-            ) < convert_to_milliseconds(timestamps[a][0]):
-                init_diff = 30000
-            elif a == len(transcript) - 1:
-                text += transcript[(timestamps[a][0], timestamps[a][1])]
-                init_diff = 30000
-                a += 1
-            else:
-                init_diff = calculate_difference(start, timestamps[b][1])
-                text += transcript[(timestamps[a][0], timestamps[a][1])]
-                b += 1
-                if convert_to_milliseconds(timestamps[b][0]) > convert_to_milliseconds(
-                    timestamps[a][1]
-                ):
-                    init_diff += calculate_difference(
-                        timestamps[b][0],
-                        timestamps[a][1],
-                    )
-                    added_silence = True
-                a += 1
+            diff = init_diff
+            text += transcript[(timestamps[b][0], timestamps[b][1])]
+            b += 1
         else:
-            new_start = adjust_timestamp(start, 30)
-            output_file = f"{output_dir}/{start}_{new_start}.txt"
-            remain_text = ""
-            if init_diff >= 31000:
-                if not added_silence:
-                    if (init_diff - 30000) // 1000 > len(
-                        transcript[(timestamps[a - 1][0], timestamps[a - 1][1])].split(
-                            " "
-                        )
-                    ):
-                        keep_len = int(
-                            np.ceil(
-                                ((init_diff - 30000) // 1000)
-                                / len(
-                                    transcript[
-                                        (timestamps[a - 1][0], timestamps[a - 1][1])
-                                    ].split(" ")
-                                )
-                                * 1.0
-                            )
-                        )
-                        tokens = text.split(" ")
-                        tokens = tokens[
-                            : -(
-                                len(
-                                    transcript[
-                                        (timestamps[a - 1][0], timestamps[a - 1][1])
-                                    ].split(" ")
-                                )
-                                - keep_len
-                            )
-                        ]
-                        text = " ".join(tokens)
-                        remain_text = transcript[
-                            (timestamps[a - 1][0], timestamps[a - 1][1])
-                        ][keep_len:]
-                    else:  # < or ==
-                        tokens = text.split(" ")
-                        text = " ".join(
-                            tokens[
-                                : -len(
-                                    transcript[
-                                        (timestamps[a - 1][0], timestamps[a - 1][1])
-                                    ].split(" ")
-                                )
-                                + 1
-                            ]
-                        )
-                        remain_text = " ".join(
-                            transcript[
-                                (timestamps[a - 1][0], timestamps[a - 1][1])
-                            ].split(" ")[1:]
-                        )
-
-                    a -= 1
-                    b -= 1
-
-            transcript_file = open(output_file, "w")
+            t_output_file = (
+                f"{t_output_dir}/{timestamps[a][0]}_{timestamps[b - 1][1]}.txt"
+            )
+            transcript_file = open(t_output_file, "w")
             transcript_file.write(text)
             transcript_file.close()
 
-            if a == len(transcript):
-                break
+            if a == 0:
+                start_window = -2
+            else:
+                start_window = 0
 
+            end_window = 0
+
+            trim_audio(
+                audio_file, timestamps[a][0], timestamps[b - 1][1], start_window, end_window, a_output_dir
+            )
+            text = ""
             init_diff = 0
-            text = remain_text
-            start = new_start
+            diff = 0
+            a = b
+
+        if b == len(transcript) and diff < 30000:
+            t_output_file = (
+                f"{t_output_dir}/{timestamps[a][0]}_{timestamps[b - 1][1]}.txt"
+            )
+            transcript_file = open(t_output_file, "w")
+            transcript_file.write(text)
+            transcript_file.close()
+
+            trim_audio(
+                audio_file, timestamps[a][0], timestamps[b - 1][1], 0, 0, a_output_dir
+            )
+
+            break
+
+
+# def chunk_transcript(transcript_file: str, output_dir: str) -> None:
+#     output_dir = output_dir + "/segments"
+#     os.makedirs(output_dir, exist_ok=True)
+
+#     transcript, *_ = read_vtt(transcript_file)
+#     a = 0
+#     b = 0
+#     timestamps = list(transcript.keys())
+#     start = timestamps[a][0]
+#     # end = timestamps[b][1]
+#     # text = transcript[(start, end)]
+#     init_diff = 0
+#     text = ""
+#     added_silence = False
+#     remain_text = ""
+
+#     while a < len(transcript) + 1:
+#         if init_diff < 30000:
+#             if init_diff == 0 and convert_to_milliseconds(
+#                 adjust_timestamp(start, 30)
+#             ) < convert_to_milliseconds(timestamps[a][0]):
+#                 init_diff = 30000
+#             elif a == len(transcript) - 1:
+#                 text += transcript[(timestamps[a][0], timestamps[a][1])]
+#                 init_diff = 30000
+#                 a += 1
+#             else:
+#                 init_diff = calculate_difference(start, timestamps[b][1])
+#                 text += transcript[(timestamps[a][0], timestamps[a][1])]
+#                 b += 1
+#                 if convert_to_milliseconds(timestamps[b][0]) > convert_to_milliseconds(
+#                     timestamps[a][1]
+#                 ):
+#                     init_diff += calculate_difference(
+#                         timestamps[b][0],
+#                         timestamps[a][1],
+#                     )
+#                     added_silence = True
+#                 a += 1
+#         else:
+#             new_start = adjust_timestamp(start, 30)
+#             output_file = f"{output_dir}/{start}_{new_start}.txt"
+#             remain_text = ""
+#             if init_diff >= 31000:
+#                 if not added_silence:
+#                     if (init_diff - 30000) // 1000 > len(
+#                         transcript[(timestamps[a - 1][0], timestamps[a - 1][1])].split(
+#                             " "
+#                         )
+#                     ):
+#                         keep_len = int(
+#                             np.ceil(
+#                                 ((init_diff - 30000) // 1000)
+#                                 / len(
+#                                     transcript[
+#                                         (timestamps[a - 1][0], timestamps[a - 1][1])
+#                                     ].split(" ")
+#                                 )
+#                                 * 1.0
+#                             )
+#                         )
+#                         tokens = text.split(" ")
+#                         tokens = tokens[
+#                             : -(
+#                                 len(
+#                                     transcript[
+#                                         (timestamps[a - 1][0], timestamps[a - 1][1])
+#                                     ].split(" ")
+#                                 )
+#                                 - keep_len
+#                             )
+#                         ]
+#                         text = " ".join(tokens)
+#                         remain_text = transcript[
+#                             (timestamps[a - 1][0], timestamps[a - 1][1])
+#                         ][keep_len:]
+#                     else:  # < or ==
+#                         tokens = text.split(" ")
+#                         text = " ".join(
+#                             tokens[
+#                                 : -len(
+#                                     transcript[
+#                                         (timestamps[a - 1][0], timestamps[a - 1][1])
+#                                     ].split(" ")
+#                                 )
+#                                 + 1
+#                             ]
+#                         )
+#                         remain_text = " ".join(
+#                             transcript[
+#                                 (timestamps[a - 1][0], timestamps[a - 1][1])
+#                             ].split(" ")[1:]
+#                         )
+
+#                     a -= 1
+#                     b -= 1
+
+#             transcript_file = open(output_file, "w")
+#             transcript_file.write(text)
+#             transcript_file.close()
+
+#             if a == len(transcript):
+#                 break
+
+#             init_diff = 0
+#             text = remain_text
+#             start = new_start
 
 
 if __name__ == "__main__":
     video_id = "-t-J098gF10"
     transcript_file = "data/transcripts/-t-J098gF10/-t-J098gF10.en-uYU-mmqFLq8.vtt"
-    output_dir = "data/transcripts/-t-J098gF10"
-    chunk_transcript(transcript_file, output_dir)
+    audio_file = "data/audio/-t-J098gF10/-t-J098gF10.wav"
+    t_output_dir = "data/transcripts/-t-J098gF10"
+    a_output_dir = "data/audio/-t-J098gF10"
+    # chunk_transcript(transcript_file, output_dir)
+    chunk_audio_transcript(transcript_file, audio_file, t_output_dir, a_output_dir)
