c0077243fb66df5f8f2eea9e3552b85c8995b119
huongngo-8 <hvn2002@uw.edu>
2024-01-31 21:53:53 +0000

    commit on main

--- scripts/training/debug.py
+++ scripts/training/debug.py
@@ -6,6 +6,9 @@ import os
 from dataclasses import dataclass
 import numpy as np
 from torch.optim import AdamW
+from torch.optim.lr_scheduler import LambdaLR
+from torch.nn.utils import clip_grad_norm_
+from torcheval.metrics import WordErrorRate
 
 AUDIO_FILE = "data/audio/eh77AUKedyM/segments/00:00:01.501_00:00:30.071.wav"
 TRANSCRIPT_FILE = "data/transcripts/eh77AUKedyM/segments/00:00:01.501_00:00:30.071.txt"
@@ -59,6 +62,30 @@ n_text_layer = 4
 # )
 # # convert text tokens to tensor
 # text_tokens = torch.tensor(text_tokens, dtype=torch.long, device=DEVICE)
+
+# transcript_dir = "data/transcripts/eh77AUKedyM/segments"
+# transcript_files = [os.path.join(transcript_dir, transcript_file) for transcript_file in sorted(os.listdir(transcript_dir))]
+# for file_index, transcript_file in enumerate(transcript_files):
+#     with open(file=transcript_file, mode="r") as f:
+#         transcript = f.read().strip()
+#     text_tokens = tokenizer.encode(transcript)
+
+#     if file_index == 0:
+#         text_tokens = (
+#             list(tokenizer.sot_sequence_including_notimestamps) + text_tokens
+#         )
+
+#     text_tokens = np.pad(
+#         text_tokens,
+#         pad_width=(0, n_text_ctx - len(text_tokens)),
+#         mode="constant",
+#         constant_values=tokenizer.no_speech,
+#     )
+
+#     if file_index == len(transcript_files) - 1:
+#         text_tokens[-1] = tokenizer.eot
+
+#     text_tokens = torch.tensor(text_tokens, dtype=torch.long, device=DEVICE)
 
 
 class AudioDataset(Dataset):
@@ -126,11 +153,12 @@ class TextDataset(Dataset):
 class AudioTextDataset(Dataset):
     def __init__(self, audio_dir, transcript_dir, tokenizer, device, n_text_ctx):
         self.audio_files = [
-            os.path.join(audio_dir, audio_file) for audio_file in os.listdir(audio_dir)
+            os.path.join(audio_dir, audio_file)
+            for audio_file in sorted(os.listdir(audio_dir))
         ]
         self.transcript_files = [
             os.path.join(transcript_dir, transcript_file)
-            for transcript_file in os.listdir(transcript_dir)
+            for transcript_file in sorted(os.listdir(transcript_dir))
         ]
         self.tokenizer = tokenizer
         self.device = device
@@ -177,9 +205,7 @@ class AudioTextDataset(Dataset):
         )
 
         if file_index == len(self.transcript_files) - 1:
-            text_tokens = (
-                text_tokens[: -len('<|nospeech|>')] + self.tokenizer.eot
-            )
+            text_tokens[-1] = tokenizer.eot
 
         text_tokens = torch.tensor(text_tokens, dtype=torch.long, device=self.device)
         return text_tokens
@@ -194,7 +220,7 @@ audio_text_dataset = AudioTextDataset(
     n_text_ctx=448,
 )
 audio_text_dataloader = DataLoader(
-    audio_text_dataset, batch_size=1, shuffle=True, num_workers=0
+    audio_text_dataset, batch_size=2, shuffle=True, num_workers=0
 )
 
 
@@ -227,21 +253,56 @@ model_dims = ModelDimensions(
 
 
 model = model.Whisper(dims=model_dims).to(DEVICE)
-optimizer = AdamW(model.parameters())
+lr = 1.5e-3
+betas = (0.9, 0.98)
+eps = 1e-6
+weight_decay = 0.1
+optimizer = AdamW(
+    model.parameters(), lr=lr, betas=betas, eps=eps, weight_decay=weight_decay
+)
+
+epochs = 10
+total_steps = len(audio_text_dataloader) * epochs
+warmup_steps = int(0.1 * total_steps)
+
+
+# def lr_lambda(current_step: int) -> float:
+#     if current_step < warmup_steps:
+#         return float(current_step) / float(max(1, warmup_steps))
+#     return max(
+#         0.0,
+#         float(total_steps - current_step) / float(max(1, total_steps - warmup_steps)),
+#     )
+
+
+# scheduler = LambdaLR(optimizer=optimizer, lr_lambda=lr_lambda)
+max_grad_norm = 1.0
+metric = WordErrorRate()
+
 model.train()
 
-for batch_idx, batch in enumerate(audio_text_dataloader):
-    optimizer.zero_grad()
-    audio_input, text_input, text_y = batch
+for epoch in range(epochs):
+    for batch_idx, batch in enumerate(audio_text_dataloader):
+        # print(f"{scheduler.get_last_lr()=}")
+        optimizer.zero_grad()
+        audio_input, text_input, text_y = batch
 
-    logits = model(audio_input, text_input)
-    # probs = F.softmax(logits, dim=-1)
-    # pred = torch.argmax(probs, dim=-1)
+        logits = model(audio_input, text_input)
+        probs = F.softmax(logits, dim=-1)
+        pred = torch.argmax(probs, dim=-1)
 
-    loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), text_y.view(-1))
+        pred_text = tokenizer.decode(list(pred[0].cpu().numpy()))
+        print(f"{pred_text=}")
+        tgt_text = tokenizer.decode(list(text_y[0].cpu().numpy()))
+        print(f"{tgt_text=}")
+        metric.update(pred_text, tgt_text)
+        wer = metric.compute().cpu().numpy().item() * 100
+        print(f"{wer}")
 
-    loss.backward()
-    optimizer.step()
-    
+        loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), text_y.view(-1))
+        print(f"{loss=}")
+        loss.backward()
+        clip_grad_norm_(model.parameters(), max_grad_norm)  # gradient clipping
 
-
+        optimizer.step()
+        # scheduler.step()
