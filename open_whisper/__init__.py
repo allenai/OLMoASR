6bf9f4325d76720fc6a0bfb888ab98f4ee65e75d
huongngo-8 <hvn2002@uw.edu>
2024-03-26 21:17:20 +0000

    commit on main

--- open_whisper/__init__.py
+++ open_whisper/__init__.py
@@ -1,4 +1,5 @@
 """Main entry point for the open_whisper package."""
 
 from open_whisper import audio, model, preprocess, tokenizer, utils
-from model import load_model
+from open_whisper.model import ModelDimensions, Whisper
+from open_whisper.audio import load_audio, log_mel_spectrogram, pad_or_trim
