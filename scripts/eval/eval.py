c896582147709a4bccba98364b1149cc26e466eb
huongngo-8 <hvn2002@uw.edu>
2024-03-26 21:39:15 +0000

    creating dataloaders for eval suite

--- scripts/eval/eval.py
+++ scripts/eval/eval.py
@@ -1,20 +1,93 @@
 # using whisper's decoding function
 import whisper
+import open_whisper as ow
+from open_whisper import utils
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+import torch.nn.functional as F
+from typing import Literal
+import os
+import numpy as np
 
-model = whisper.load_model("checkpoints/tiny-en.pt")
 
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio(
-    "data/sanity-check/audio/eh77AUKedyM/segments/00:00:01.501_00:00:30.071.m4a"
-)
-audio = whisper.pad_or_trim(audio)
+class librispeech:
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
 
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    def load(self):
+        transcript_files = []
+        audio_text = {}
+        for root, _, files in os.walk(self.root_dir):
+            for file in files:
+                if file.endswith(".txt"):
+                    transcript_files.append(os.path.join(root, file))
 
-options = whisper.DecodingOptions(language="en", without_timestamps=True)
+        for file in sorted(transcript_files):
+            with open(file, "r") as f:
+                for line in f:
+                    audio_codes = line.split(" ")[0].split("-")
+                    audio_file = os.path.join(
+                        self.rootdir,
+                        audio_codes[0],
+                        audio_codes[1],
+                        f"{audio_codes[0]}-{audio_codes[1]}-{audio_codes[2]}.flac",
+                    )
+                    audio_text[audio_file] = " ".join(line.split(" ")[1:]).strip()
 
-result = whisper.decode(model, mel, options)
+        return list(audio_text.keys()), list(audio_text.values())
 
-print(result.text)
 
+class artie_bias_corpus:
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+
+    def load(self):
+        audio_files = []
+        transcript_texts = []
+        with open(os.path.join(self.root_dir, "artie-bias-corpus.tsv"), "r") as f:
+            for line in f:
+                audio_file = os.path.join(self.root_dir, line.split("\t")[1].strip())
+                transcript_text = line.split("\t")[2].strip()
+                audio_files.append(audio_file)
+                transcript_texts.append(transcript_text)
+
+        return audio_files, transcript_texts
+
+
+class AudioTextDataset(Dataset):
+    def __init__(
+        self,
+        corpus: Literal["librispeech", "artie_bias_corpus"],
+    ):
+        self.corpus = corpus
+
+        if corpus == "librispeech":
+            audio_files, transcript_texts = librispeech.load()
+        elif corpus == "artie_bias_corpus":
+            audio_files, transcript_texts = artie_bias_corpus.load()
+
+        self.audio_files = audio_files
+        self.transcript_texts = transcript_texts
+
+    def __len__(self):
+        return len(self.audio_files)
+
+    def __getitem__(self, index):
+        # not sure if putting it here is bad...
+        audio_file, audio_input = self.preprocess_audio(self.audio_files[index])
+        text_y = self.transcript_texts[index]
+        return (
+            audio_file,
+            audio_input,
+            text_y,
+        )
+
+    def preprocess_audio(self, audio_file):
+        audio_arr = audio.load_audio(audio_file, sr=16000)
+        audio_arr = audio.pad_or_trim(audio_arr)
+        mel_spec = audio.log_mel_spectrogram(audio_arr)
+        # might need to adjust this to a softer threshold instead of exact match
+        # if sum(audio_arr) != 0.0:
+        #     mel_spec_normalized = (mel_spec - mel_spec.mean()) / "mel_spec.std()
+        #     mel_spec_scaled = mel_spec_normalized / (mel_spec_normalized.abs().max())
+        return audio_file, mel_spec
