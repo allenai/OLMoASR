08dce6881033efc4f44dc87e22f4ce5cdf0193e8
huongngo-8 <hvn2002@uw.edu>
2024-03-28 00:22:42 +0000

    adding decoding to main entry point for package

--- open_whisper/__init__.py
+++ open_whisper/__init__.py
@@ -1,5 +1,5 @@
 """Main entry point for the open_whisper package."""
 
-from open_whisper import audio, model, preprocess, tokenizer, utils
+from open_whisper import audio, model, preprocess, tokenizer, utils, decoding
 from open_whisper.model import ModelDimensions, Whisper
 from open_whisper.audio import load_audio, log_mel_spectrogram, pad_or_trim
