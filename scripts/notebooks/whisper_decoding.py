271baca765f6f8d447c5ebfd663d8203c5b13822
huongngo-8 <hvn2002@uw.edu>
2024-03-28 04:01:59 +0000

    inference notebooks

---
+++ scripts/notebooks/whisper_decoding.py
@@ -0,0 +1,92 @@
+# %%
+import torch
+import torch.nn.functional as F
+from open_whisper import (
+    ModelDimensions,
+    load_audio,
+    log_mel_spectrogram,
+    pad_or_trim,
+    Whisper,
+    tokenizer,
+)
+from whisper import whisper
+
+# %%
+fp = "/home/ubuntu/open_whisper/checkpoints/whisper/tiny-en-whisper.pt"
+model = whisper.load_model(fp)
+
+# %%
+device = torch.device("cuda")
+
+# %%
+model.to(device)
+
+# %%
+audio = whisper.load_audio(
+    "/home/ubuntu/open_whisper/data/eval/artie-bias-corpus/common_voice_en_250.mp3"
+)
+audio = whisper.pad_or_trim(audio)
+audio_input = whisper.log_mel_spectrogram(audio).to(model.device)
+
+# %%
+options = whisper.DecodingOptions(language="en", without_timestamps=True)
+
+# %%
+result = whisper.decode(model, audio_input, options)
+result.text
+
+# %%
+batch_audio_input = audio_input.view(1, audio_input.shape[0], -1)
+
+# %%
+temp_tokenizer = whisper.tokenizer.get_tokenizer(
+    multilingual=True, language="en", task="transcribe"
+)
+
+# %%
+temp_tokenizer.sot_sequence_including_notimestamps
+
+# %%
+temp_tokenizer.eot
+
+# %%
+decoder_input = torch.full((1, 1), 50258, dtype=torch.long, device=device)
+
+# %%
+generated_sequences = [[]]
+active = torch.ones(1, dtype=torch.bool)
+
+# %%
+logits = model(batch_audio_input, decoder_input)
+probs = F.softmax(logits, dim=-1)
+# not a 1-dim tensor! grows as decoding continues
+next_token_pred = torch.argmax(probs, dim=-1)
+next_token_pred
+
+# %%
+decoder_input = torch.tensor([[50258, 50259, 50359, 50363]], device=device)
+decoder_input
+
+# %%
+logits = model(batch_audio_input, decoder_input)
+probs = F.softmax(logits, dim=-1)
+# not a 1-dim tensor! grows as decoding continues
+next_token_pred = torch.argmax(probs, dim=-1)
+next_token_pred
+
+# %%
+probs.shape
+
+# %%
+decoder_input = torch.tensor([[50258, 50259]], device=device)
+decoder_input
+
+# %%
+logits = model(batch_audio_input, decoder_input)
+probs = F.softmax(logits, dim=-1)
+# not a 1-dim tensor! grows as decoding continues
+next_token_pred = torch.argmax(probs, dim=-1)
+next_token_pred
+
+# %%
+model.eval()
