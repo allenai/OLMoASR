5f674132afcc32286cf1dbcd68b47752f2021cf3
huongngo-8 <hvn2002@uw.edu>
2024-04-01 23:55:57 +0000

    added decoding and transcribe functionality to model code

--- open_whisper/inf_model.py
+++ open_whisper/inf_model.py
@@ -8,6 +8,10 @@ import torch
 import torch.nn.functional as F
 from torch import Tensor, nn
 
+from .decoding import decode as decode_function
+from .decoding import detect_language as detect_language_function
+from .transcribe import transcribe as transcribe_function
+
 
 @dataclass
 class ModelDimensions:
@@ -371,3 +375,7 @@ class Whisper(nn.Module):
 
         self.decoder.apply(install_hooks)
         return cache, hooks
+
+    detect_language = detect_language_function
+    transcribe = transcribe_function
+    decode = decode_function
