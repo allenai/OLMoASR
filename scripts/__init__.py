8ae4916f11b772f31eada9fb1ea220448c260758
huongngo-8 <hvn2002@uw.edu>
2024-04-28 04:24:02 +0000

    fixed circular import issues

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,5 +1,1 @@
 from scripts.training.train import AudioTextDataset
-from scripts.eval.libri_artie import AudioTextDataset as EvalAudioTextDataset
-from scripts.data.filtering.get_filter_mechs import get_manual_ids
-from scripts.training import for_logging
-from scripts.data.filtering.get_filter_mechs import get_baseline_data, get_manual_data
