d47bfb6d44aebf16f4ecaea26333e2705d892e49
Huong Viet Ngo <hvn2002@g3010.hyak.local>
2024-04-29 18:37:07 -0700

    new arguments for shell scripts to run experiments

--- experiments/ow_tiny.sh
+++ experiments/ow_tiny.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=caps_mg
+#SBATCH --job-name=ow_tiny
 #SBATCH --partition=gpu-a40
 #SBATCH --account=efml
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=180G
-#SBATCH --gres=gpu:6
+#SBATCH --gres=gpu:5
 #SBATCH --time=120:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hvn2002@uw.edu
@@ -18,11 +18,11 @@ cd /mmfs1/gscratch/efml/hvn2002/open_whisper
 cat $0
 echo "--------------------"
 
-torchrun --nnodes 1 --nproc_per_node 6 scripts/training/train.py \
+torchrun --nnodes 1 --nproc_per_node 5 scripts/training/train.py \
     --model_variant=tiny \
-    --exp_name=caps_mg \
+    --exp_name=ow_tiny \
     --job_type=train \
-    --filter=manual \
+    --filter=baseline \
     --run_id=None \
     --rank=None \
     --world_size=None \
@@ -31,14 +31,14 @@ torchrun --nnodes 1 --nproc_per_node 6 scripts/training/train.py \
     --eps=1e-6 \
     --weight_decay=0.1 \
     --max_grad_norm=1.0 \
-    --subset=None \
-    --epochs=14 \
+    --subset=5000 \
+    --epochs=8 \
     --eff_size=256 \
-    --train_batch_size=32 \
+    --train_batch_size=64 \
     --val_batch_size=32 \
     --eval_batch_size=32 \
     --train_val_split=0.99 \
-    --num_workers=36 \
+    --num_workers=40 \
     --pin_memory=True \
     --shuffle=True \
     --persistent_workers=True \
