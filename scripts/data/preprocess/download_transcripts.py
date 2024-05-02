d18cfd7f422e0f7f9d2c59218fe9a35b56873d84
Huong Viet Ngo <hvn2002@g3054.hyak.local>
2024-05-02 15:39:40 -0700

    refactored download transcripts code to use main func and console args

--- scripts/data/preprocess/download_transcripts.py
+++ scripts/data/preprocess/download_transcripts.py
@@ -9,105 +9,48 @@ from tqdm import tqdm
 from itertools import repeat
 import os
 import numpy as np
-from tqdm import tqdm
-
-transcript_ext = "srt"
-
-# --- sanity-check example ---
-# reading in metadata
-# df = pd.read_parquet("data/metadata/captions-0010.parquet")
-# only getting english data (that's less than 5 minutes long)
-# en_df = df[
-#     (df["manual_caption_languages"].str.contains("en"))
-#     & (df["automatic_caption_orig_language"].str.contains("en"))
-# ]
-
-# rng = np.random.default_rng(42)
-
-# sample = en_df[en_df["categories"] == "Education"][
-#     ["id", "manual_caption_languages"]
-# ].to_numpy()
-# sample = rng.choice(sample, 10, replace=False)
-# print(sample)
-
-# # ensuring that language codes are english only
-# for i, (id, langs) in enumerate(sample):
-#     if "," in langs:
-#         for lang in langs.split(","):
-#             if "en" in lang:
-#                 sample[i][1] = lang
-#                 break
-
-# metadata_files = []
-# for f in os.listdir("data/metadata"):
-#     metadata_files.append(os.path.join("data/metadata", f))
-
-# df_list = []
-# for f in metadata_files:
-#     captions_df = pd.read_parquet(f)
-#     df_list.append(
-#         captions_df[
-#             ["id", "manual_caption_languages", "automatic_caption_orig_language"]
-#         ]
-#     )
-
-id_list = []
-lang_list = []
-
-for f in tqdm(os.listdir("logs/data")):
-    if "en_id" in f:
-        with open(f"logs/data/{f}", "r") as file:
-            for line in file:
-                id_list.append(line.strip().split(" ")[0])
-                lang_list.append(line.strip().split(" ")[1])
+from datetime import datetime
+from fire import Fire
 
 
-sample_id = os.listdir("data/audio")
-sample_lang = []
+def main(samples_file: str):
+    """
+    Download transcript files from a file of samples.
+    Has to be in the format of:
+        `video_id \\t manual_caption_languages \\t automatic_caption_langage \\t duration`
 
+    Parameters
+    ----------
+    samples_file: str
+        Path to the file containing the samples
 
-def get_lang_for_id(video_id):
-    idx = id_list.index(video_id)
-    langs = lang_list[idx]
-    if "," in langs:
-        lang = langs.split(",")[0]
-    else:
-        lang = langs
-    with open("logs/data/sample_id_lang.txt", "a") as f:
-        f.write(f"{video_id} {lang}\n")
-    return (video_id, lang)
-
-
-with multiprocessing.Pool() as pool:
-    sample_id_lang = list(
-        tqdm(
-            pool.imap_unordered(get_lang_for_id, sample_id, chunksize=10),
-            total=len(sample_id),
+    Returns
+    -------
+    None
+    """
+    transcript_ext = "srt"
+    with open(samples_file, "r") as f:
+        sample_id, sample_lang = list(
+            zip(*[line.strip().split("\t")[:2] for line in f])
         )
-    )
 
-with open("logs/data/sample_id_lang.txt", "w") as f:
-    for item in sample_id_lang:
-        f.write(f"{item[0]} {item[1]}\n")
-
-sample_id, sample_lang = zip(*sample_id_lang)
-print(sample_id[:10])
-print(sample_lang[:10])
-
-
-with multiprocessing.Pool() as pool:
-    out = list(
-        tqdm(
-            pool.imap_unordered(
-                parallel_download_transcript,
-                zip(
-                    sample_id,
-                    sample_lang,
-                    repeat("data/transcripts"),
-                    repeat(transcript_ext),
+    with multiprocessing.Pool() as pool:
+        out = list(
+            tqdm(
+                pool.imap_unordered(
+                    parallel_download_transcript,
+                    zip(
+                        sample_id,
+                        sample_lang,
+                        repeat("data/transcripts"),
+                        repeat(transcript_ext),
+                    ),
+                    chunksize=50,
                 ),
-                chunksize=20,
-            ),
-            total=len(sample_id),
+                total=len(sample_id),
+            )
         )
-    )
+
+
+if __name__ == "__main__":
+    Fire(main)
