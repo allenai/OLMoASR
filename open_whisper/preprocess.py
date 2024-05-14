4e6ca4682906a8d2076e2055d3ccdae9d889990b
huongngo-8 <hvn2002@uw.edu>
2024-05-14 19:33:45 +0000

    updated identifier for ip blocking

--- open_whisper/preprocess.py
+++ open_whisper/preprocess.py
@@ -134,7 +134,7 @@ def download_audio(
         with open(f"metadata/{output_dir}/unavailable_videos.txt", "a") as f:
             f.write(f"{video_id}\n")
             return None
-    elif not os.path.exists(f"{output_dir}/{video_id}/{video_id}.{ext}"):
+    elif "HTTP Error 403" in result.stderr:
         with open(f"metadata/{output_dir}/blocked_ip.txt", "a") as f:
             f.write(f"{video_id}\n")
         return "requeue"
