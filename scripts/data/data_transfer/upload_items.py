b782be3f2f4ee90adb0632b13903fced7fa68f62
huongngo-8 <hvn2002@uw.edu>
2024-05-16 03:21:44 +0000

    updated uploading items to queuemanager to only uploaded items that haven't been processed

--- scripts/data/data_transfer/upload_items.py
+++ scripts/data/data_transfer/upload_items.py
@@ -1,5 +1,7 @@
 from moto3.queue_manager import QueueManager
+from moto3.s3_manager import S3Manager
 
+s3m = S3Manager("mattd-public/whisper")
 qm = QueueManager("whisper-downloading")
 
 # make the items
@@ -7,8 +9,13 @@ items = []
 with open("logs/data/download/sampled_en.txt", "r") as f:
     id_lang = [line.strip().split("\t")[:2] for line in f]
 
+uploaded = []
+for f in s3m.list_all_files():
+    if "tar.gz" in f:
+        uploaded.append(int(f.split(".")[0]))
+
 for i in range(0, len(id_lang), 1000):
-    if i // 1000 in [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 13, 14, 15, 21, 36]:
+    if i // 1000 in uploaded:
         pass
     else:
         item = {"id_lang": id_lang[i : i + 1000], "group_id": f"{((i // 1000)):08}"}
