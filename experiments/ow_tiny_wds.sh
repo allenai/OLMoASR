72fc3257602e08c876e4ebd8a60c72b3604af1bb
huongngo-8 <hvn2002@uw.edu>
2024-08-06 10:19:28 -0700

    removed epoch dependency and added debug statements

--- experiments/ow_tiny_wds.sh
+++ experiments/ow_tiny_wds.sh
@@ -19,7 +19,11 @@ cd /mmfs1/gscratch/efml/hvn2002/open_whisper
 cat $0
 echo "--------------------"
 
-torchrun --nnodes 1 --nproc_per_node 1 scripts/training/train_wds.py \
+export TORCH_NCCL_BLOCKING_WAIT=1
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_DEBUG=INFO
+
+torchrun --nnodes 1 --nproc_per_node 6 scripts/training/train_wds.py \
     --model_variant=tiny \
     --exp_name=ow_tiny_wds \
     --job_type=train \
@@ -35,12 +39,11 @@ torchrun --nnodes 1 --nproc_per_node 1 scripts/training/train_wds.py \
     --eps=1e-6 \
     --weight_decay=0.1 \
     --max_grad_norm=1.0 \
-    --epochs=2 \
-    --eff_size=256 \
-    --train_batch_size=8 \
-    --val_batch_size=8 \
-    --eval_batch_size=8 \
-    --num_workers=45 \
+    --eff_batch_size=256 \
+    --train_batch_size=16 \
+    --val_batch_size=16 \
+    --eval_batch_size=16 \
+    --num_workers=6 \
     --pin_memory=True \
     --persistent_workers=True \
     --run_val=True \
