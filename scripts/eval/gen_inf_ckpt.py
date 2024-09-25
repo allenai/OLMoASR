7a4ab330c4f9c8b9def9ce4d28eae57aadd619ac
huongngo-8 <hvn2002@uw.edu>
2024-09-25 15:50:55 -0700

    script to modify non-ddp model ckpt for inference

---
+++ scripts/eval/gen_inf_ckpt.py
@@ -0,0 +1,12 @@
+import torch
+from fire import Fire
+
+def gen_inf_ckpt(ckpt_path: str, save_path: str):
+    checkpoint = torch.load(ckpt_path)
+    new_dec_tok_emb_w = checkpoint["model_state_dict"]["decoder.token_embedding.weight"][:-1]
+    checkpoint["model_state_dict"]["decoder.token_embedding.weight"] = new_dec_tok_emb_w
+    torch.save(checkpoint, save_path)
+    print(f"Saved inference checkpoint to {save_path}")
+
+if __name__ == "__main__":
+    Fire(gen_inf_ckpt)
