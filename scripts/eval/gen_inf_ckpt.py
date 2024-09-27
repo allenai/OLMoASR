f7748e4a47f74197a0990aa7df4521a71e4f3cbc
huongngo-8 <hvn2002@uw.edu>
2024-09-26 18:42:05 -0700

    update with changing data type for dims attribute in inf ckpts

--- scripts/eval/gen_inf_ckpt.py
+++ scripts/eval/gen_inf_ckpt.py
@@ -5,6 +5,7 @@ def gen_inf_ckpt(ckpt_path: str, save_path: str):
     checkpoint = torch.load(ckpt_path)
     new_dec_tok_emb_w = checkpoint["model_state_dict"]["decoder.token_embedding.weight"][:-1]
     checkpoint["model_state_dict"]["decoder.token_embedding.weight"] = new_dec_tok_emb_w
+    checkpoint["dims"] = checkpoint["dims"].__dict__
     torch.save(checkpoint, save_path)
     print(f"Saved inference checkpoint to {save_path}")
     return save_path
