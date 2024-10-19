f0f8984d01227087a1c6888af030d29f24749458
huongngo-8 <hvn2002@uw.edu>
2024-10-19 16:36:44 -0700

    updated downloading command in download_gcs

--- scripts/data/data_transfer/download_gcs.py
+++ scripts/data/data_transfer/download_gcs.py
@@ -24,15 +24,19 @@ def download_files(
     os.makedirs(local_dir, exist_ok=True)
     os.makedirs(os.path.dirname(log_file), exist_ok=True)
     start_idx = int(os.getenv("BEAKER_REPLICA_RANK")) * batch_size
-    end_idx = (start_idx + batch_size) - 1
-    cmd = f"gcloud auth activate-service-account '{service_account}' --key-file='{key_file}' && gsutil -m cp -L {log_file} -r gs://{bucket_name}/{bucket_prefix}/[{start_idx:04}-{end_idx:04}].tar.gz {local_dir}",
-    try:
-        result = subprocess.run(cmd, shell=True, capture_output=True)
-        logger.info(result.stdout)
-        logger.info(result.stderr)
-    except Exception as e:
-        logger.info(result.stderr)
-        logger.error(e)
+    end_idx = start_idx + batch_size
+
+    for file_idx in range(start_idx, end_idx):
+        cmd = (
+            f"gcloud auth activate-service-account '{service_account}' --key-file='{key_file}' && gsutil -m cp -L {log_file} -r gs://{bucket_name}/{bucket_prefix}/{file_idx:04}.tar.gz {local_dir}",
+        )
+        try:
+            result = subprocess.run(cmd, shell=True, capture_output=True)
+            logger.info(result.stdout)
+            logger.info(result.stderr)
+        except Exception as e:
+            logger.info(result.stderr)
+            logger.error(e)
 
 
 if __name__ == "__main__":
