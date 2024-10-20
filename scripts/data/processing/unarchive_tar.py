c57d58d7d7b5d6640066a33542e727f4d58572a4
huongngo-8 <hvn2002@uw.edu>
2024-10-19 18:23:25 -0700

    fixed another bug w/ creating index in unarchive_tar

--- scripts/data/processing/unarchive_tar.py
+++ scripts/data/processing/unarchive_tar.py
@@ -52,11 +52,11 @@ def unarchive_tar(
             batch_idx * batch_size : (batch_idx * batch_size) + batch_size
         ]
     )
+    start_dir_idx = start_dir_idx + ((batch_size / group_by) * batch_idx)
     tar_files_dir_idx = [
         (batch_tar[i : i + group_by], f"{(start_dir_idx + (i // group_by)):05}")
         for i in range(0, len(batch_tar), group_by)
     ]
-    start_dir_idx = start_dir_idx + (len(tar_files_dir_idx) * batch_idx)
 
     with multiprocessing.Pool() as pool:
         res = list(
