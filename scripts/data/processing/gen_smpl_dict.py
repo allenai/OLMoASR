549f5186c833751ba3df785ff9460b3a98dde287
huongngo-8 <hvn2002@uw.edu>
2024-10-22 16:08:43 -0700

    removed ray usage to generate samples dicts.

--- scripts/data/processing/gen_smpl_dict.py
+++ scripts/data/processing/gen_smpl_dict.py
@@ -1,11 +1,12 @@
-import ray
-from ray.data.datasource import FilenameProvider
 import glob
 import os
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
 from fire import Fire
 from itertools import chain
 import logging
+import multiprocessing
+from tqdm import tqdm
+import json
 
 logging.basicConfig(
     level=logging.INFO,
@@ -15,23 +16,8 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-class FilenameProviders:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    class SamplesDictsFilenameProvider(FilenameProvider):
-        def __init__(self, file_format: str):
-            self.file_format = file_format
-
-        def get_filename_for_block(self, block, task_index, block_index):
-            return f"samples_dicts.{self.file_format}"
-
-
-def gen_smpl_dict(row: Optional[Dict[str, Any]]) -> Dict[str, Any]:
-    segs_dir = os.path.dirname(row["item"]).replace("440K_full", "440K_seg")
-
-    if int(row["item"].split("/")[-2]) > 2448:
+def gen_smpl_dict(segs_dir) -> List:
+    if int(segs_dir.split("/")[-2]) > 2448:
         text_files = sorted(glob.glob(segs_dir + "/*.vtt"))
     else:
         text_files = sorted(glob.glob(segs_dir + "/*.srt"))
@@ -40,13 +26,10 @@ def gen_smpl_dict(row: Optional[Dict[str, Any]]) -> Dict[str, Any]:
     smpl_dicts = []
 
     for text_fp, npy_fp in text_npy_samples:
-        smpl_dict = {"key": segs_dir, "transcript": text_fp, "audio": npy_fp}
+        smpl_dict = {"transcript": text_fp, "audio": npy_fp}
         smpl_dicts.append(smpl_dict)
 
-    row["sample_dicts"] = smpl_dicts
-    del row["item"]
-
-    return row
+    return smpl_dicts
 
 
 def main(
@@ -55,6 +38,10 @@ def main(
     batch_size: int,
 ):
     batch_idx = int(os.getenv("BEAKER_REPLICA_RANK"))
+    samples_dicts_dir = samples_dicts_dir + f"/{batch_idx:03}"
+    os.makedirs(samples_dicts_dir, exist_ok=True)
+    logger.info(f"{samples_dicts_dir=}")
+    
     with open(shard_metadata, "r") as f:
         shard_metadata = [line.strip() for line in f]
         
@@ -62,16 +49,14 @@ def main(
     logger.info(f"{shard_dirs[0]=}, {shard_dirs[-1]=}, {len(shard_dirs)=}")
     data_dirs = list(chain(*[glob.glob(shard_dir + "/*") for shard_dir in shard_dirs]))
     logger.info(f"{data_dirs[:5]=}")
-    ray.init()
 
-    logger.info("Generating sample dicts")
-    ds = ray.data.from_items(data_dirs).map(gen_smpl_dict)
+    with multiprocessing.Pool() as pool:
+        smpl_dicts = list(tqdm(pool.imap_unordered(gen_smpl_dict, data_dirs), total=len(data_dirs)))
 
     logger.info("Writing sample dicts to disk")
-    ds.repartition(num_blocks=1).write_json(
-        samples_dicts_dir,
-        filename_provider=FilenameProviders.SamplesDictsFilenameProvider("jsonl"),
-    )
+    with open(f"{samples_dicts_dir}/samples_dicts.jsonl", "w") as f:
+        for smpl_dict in smpl_dicts:
+            f.write(json.dumps({"sample_dicts": smpl_dict}) + "\n")
 
 if __name__ == "__main__":
     Fire(main)
