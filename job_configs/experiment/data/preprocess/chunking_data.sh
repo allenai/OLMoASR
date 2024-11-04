795dd63b75c92843adbf35818d5108c016f6c171
huongngo-8 <hvn2002@uw.edu>
2024-11-03 23:03:05 -0800

    config for job to segment data

---
+++ job_configs/experiment/data/preprocess/chunking_data.sh
@@ -0,0 +1,38 @@
+set -ex
+
+OUTPUT_DIR="/weka/huongn/440K_seg"
+SOURCE_DIR="/weka/huongn/440K_full"
+LOG_DIR="/results/huongn"
+PREPROC_FAIL_DIR="/weka/huongn/seg_fail"
+MISSING_PAIR_DIR="/weka/huongn/missing_pairs"
+JOBS_BATCH_SIZE=1
+START_SHARD_IDX=3985
+END_SHARD_IDX=3985
+IN_MEMORY=True
+JOB_BATCH_IDX=0
+
+gantry run \
+  --name "chunking_data_3985_11_21" \
+  --description "chunking audio-transcript pairs" \
+  --allow-dirty \
+  --no-nfs \
+  --preemptible \
+  --beaker-image huongn/data_chunking \
+  --workspace ai2/open-whisper \
+  --cluster ai2/neptune-cirrascale \
+  --cpus 4.5 \
+  --budget ai2/oe-data \
+  --replicas ${JOBS_BATCH_SIZE} \
+  --weka oe-data-default:/weka \
+  --env JOB_BATCH_IDX=${JOB_BATCH_IDX} \
+  -- /bin/bash -c "python scripts/data/processing/preprocess_flat.py \
+    --output_dir=${OUTPUT_DIR} \
+    --source_dir=${SOURCE_DIR} \
+    --log_dir=${LOG_DIR} \
+    --preproc_fail_dir=${PREPROC_FAIL_DIR} \
+    --missing_pair_dir=${MISSING_PAIR_DIR} \
+    --jobs_batch_size=${JOBS_BATCH_SIZE} \
+    --start_shard_idx=${START_SHARD_IDX} \
+    --end_shard_idx=${END_SHARD_IDX} \
+    --in_memory=${IN_MEMORY}
+    " 
