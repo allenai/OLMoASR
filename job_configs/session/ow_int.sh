1b87c8b51bcbe3252c2be7412b01ae069b1d2d8e
huongngo-8 <hvn2002@uw.edu>
2024-11-04 00:15:19 -0800

    updated args for configs for interactive jobs

--- job_configs/session/ow_int.sh
+++ job_configs/session/ow_int.sh
@@ -1,13 +1,13 @@
 beaker session create \
---gpus 8 \
+--gpus 2 \
 --budget ai2/prior \
 --bare \
---image beaker://huongn/ow_train_rand_noreslog_acc \
---cpus 64.0 \
---memory 20GiB \
+--image beaker://huongn/ow_train_debug \
+--cpus 62.0 \
 --mount beaker://huongn/mini-job-ow-dataset=/wds_shards \
 --mount beaker://huongn/mini-job-ow-evalset=/ow_eval \
---name ow_thruput_rand_noreslog_acc \
+--mount weka://oe-data-default=/weka \
+--name ow_train_debug \
 --priority normal \
 --result /data/huongn/ow_logs \
 --workspace ai2/open-whisper \
