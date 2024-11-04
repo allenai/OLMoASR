79bb6f730b9ac3f2e6458e0be47e13170793bbb1
huongngo-8 <hvn2002@uw.edu>
2024-11-04 00:14:47 -0800

    config for job to identify English only transcript

---
+++ job_configs/experiment/data/preprocess/filter_en_only.sh
@@ -0,0 +1,30 @@
+set -ex
+
+SOURCE_DIR="/weka/huongn/440K_full"
+LOG_DIR="/weka/huongn"
+START_SHARD_IDX=0
+END_SHARD_IDX=8448
+BATCH_SIZE=704
+NUM_REPLICAS=12
+
+gantry run \
+  --name "filter_en_only" \
+  --description "Filtering for English transcripts only" \
+  --allow-dirty \
+  --no-nfs \
+  --preemptible \
+  --pip requirements-filter.txt \
+  --workspace ai2/open-whisper \
+  --cluster ai2/jupiter-cirrascale-2 \
+  --cpus 10 \
+  --priority normal \
+  --budget ai2/oe-data \
+  --replicas ${NUM_REPLICAS} \
+  --weka oe-data-default:/weka \
+  -- /bin/bash -c "python scripts/data/filtering/filter_en_only.py \
+    --source_dir=${SOURCE_DIR} \
+    --log_dir=${LOG_DIR} \
+    --start_shard_idx=${START_SHARD_IDX} \
+    --end_shard_idx=${END_SHARD_IDX} \
+    --batch_size=${BATCH_SIZE} \
+    "
