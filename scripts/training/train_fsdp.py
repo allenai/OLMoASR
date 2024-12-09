fa16c2653b4f1ffa977aa028157fe67b487eb3fb
huongngo-8 <hvn2002@uw.edu>
2024-12-08 22:40:47 -0800

    removing autocast (to see if that's causing NaN train loss)

--- scripts/training/train_fsdp.py
+++ scripts/training/train_fsdp.py
@@ -1028,32 +1028,32 @@ def train(
         start_step = time.time()
 
         with set_detect_anomaly(mode=detect_anomaly):
-            with autocast(device_type="cuda"):
-                (
-                    audio_files,
-                    transcript_files,
-                    padded_audio_arr,
-                    audio_input,
-                    text_input,
-                    text_y,
-                    padding_mask,
-                ) = batch
+            # with autocast(device_type="cuda"):
+            (
+                audio_files,
+                transcript_files,
+                padded_audio_arr,
+                audio_input,
+                text_input,
+                text_y,
+                padding_mask,
+            ) = batch
 
-                audio_input = audio_input.to(rank)
-                text_input = text_input.to(rank)
-                text_y = text_y.to(rank)
-                padding_mask = padding_mask.to(rank)
+            audio_input = audio_input.to(rank)
+            text_input = text_input.to(rank)
+            text_y = text_y.to(rank)
+            padding_mask = padding_mask.to(rank)
 
-                logits = model(audio_input, text_input, padding_mask, verbose)
+            logits = model(audio_input, text_input, padding_mask, verbose)
 
-                train_loss = F.cross_entropy(
-                    logits.view(-1, logits.shape[-1]),
-                    text_y.view(-1),
-                    ignore_index=51864,
-                )
-                train_loss = (
-                    train_loss / accumulation_steps
-                )  # normalization of loss (gradient accumulation)
+            train_loss = F.cross_entropy(
+                logits.view(-1, logits.shape[-1]),
+                text_y.view(-1),
+                ignore_index=51864,
+            )
+            train_loss = (
+                train_loss / accumulation_steps
+            )  # normalization of loss (gradient accumulation)
 
             scaler.scale(train_loss).backward()  # accumulate gradients
             for name, param in model.named_parameters():
