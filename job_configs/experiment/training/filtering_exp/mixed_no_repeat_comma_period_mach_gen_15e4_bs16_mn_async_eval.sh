5bd0862f0d4091c8d7554f1b32025788e9c24ac3
huongngo-8 <hvn2002@uw.edu>
2024-12-17 16:27:12 -0800

    updated multi-node, fsdp job config

--- job_configs/experiment/training/filtering_exp/mixed_no_repeat_comma_period_mach_gen_15e4_bs16_mn_async_eval.sh
+++ job_configs/experiment/training/filtering_exp/mixed_no_repeat_comma_period_mach_gen_15e4_bs16_mn_async_eval.sh
@@ -6,20 +6,22 @@ LEARNING_RATE=1.5e-3
 LR="15e4"
 AUDIO_HOURS=440
 BATCH_SIZE=16
-EFFECTIVE_BATCH_SIZE=256
+EFFECTIVE_BATCH_SIZE=512
 EPOCHS=5
-DATE="121524"
-EXP_NAME="${FILTER}_${MODEL_SIZE}_${LR}_${AUDIO_HOURS}K_bs${BATCH_SIZE}_ebs${EFFECTIVE_BATCH_SIZE}_${EPOCHS}pass_${DATE}"
+NUM_WORKERS=8
+DATE="121724"
+SHARDING_STRATEGY="_HYBRID_SHARD_ZERO2"
+EXP_NAME="${FILTER}_${MODEL_SIZE}_${LR}_${AUDIO_HOURS}K_bs${BATCH_SIZE}_ebs${EFFECTIVE_BATCH_SIZE}_${NUM_WORKERS}workers_${EPOCHS}pass_${SHARDING_STRATEGY}_${DATE}_2"
 
 GPU_COUNT=8
-REPLICAS=2
+REPLICAS=2 
+SHARED_MEMORY="10.0GiB"
 
 JOB_TYPE="filtering"
 SAMPLES_DICTS_DIR="/weka/huongn/samples_dicts/filtered/mixed_no_repeat_min_comma_period_full_1_2_3_4"
-TRAIN_STEPS=1048576
-EPOCH_STEPS=206699
+TRAIN_STEPS=524288 #1048576 883012
+EPOCH_STEPS=103350 #206699 174063
 EVAL_BATCH_SIZE=8
-NUM_WORKERS=8
 RUN_EVAL="True"
 EVAL_SETS="librispeech_clean,librispeech_other"
 TRAIN_LOG_FREQ=2000
@@ -42,8 +44,10 @@ gantry run \
   --cluster ai2/jupiter-cirrascale-2 \
   --gpus ${GPU_COUNT} \
   --replicas ${REPLICAS} \
+  --shared-memory ${SHARED_MEMORY} \
   --leader-selection \
   --propagate-failure \
+  --propagate-preemption \
   --synchronized-start-timeout "30m" \
   --host-networking \
   --beaker-image huongn/ow_train_gantry \
@@ -56,6 +60,8 @@ gantry run \
   --env-secret HF_TOKEN=HF_TOKEN \
   --env WANDB_DIR=/results/huongn/ow_logs \
   --env TORCH_NCCL_BLOCKING_WAIT=1 \
+  --env NCCL_SOCKET_IFNAME=ib \
+  --env NCCL_IB_HCA=^=mlx5_bond_0 \
   --env NCCL_DEBUG=INFO \
   -- /bin/bash -c "torchrun --nnodes ${REPLICAS}:${REPLICAS} --nproc_per_node ${GPU_COUNT} --rdzv_id=101 --rdzv_backend=c10d --rdzv_endpoint=\${BEAKER_LEADER_REPLICA_HOSTNAME}:29400 scripts/training/train_fsdp.py \
       --model_variant=${MODEL_SIZE} \
@@ -80,7 +86,7 @@ gantry run \
       --val_batch_size=16 \
       --eval_batch_size=${EVAL_BATCH_SIZE} \
       --train_val_split=1.0 \
-      --num_workers=8 \
+      --num_workers=${NUM_WORKERS} \
       --pin_memory=True \
       --persistent_workers=True \
       --run_val=False \
@@ -95,7 +101,8 @@ gantry run \
       --detect_anomaly=${DETECT_ANOMALY} \
       --add_module_hooks=${ADD_MODULE_HOOKS} \
       --precision=${PRECISION} \
-      --use_orig_params=${USE_ORIG_PARAMS}"
+      --use_orig_params=${USE_ORIG_PARAMS} \
+      --sharding_strategy=${SHARDING_STRATEGY}"
 
 # 2^20 = 1048576
 # latest filter: 52740400 segments -> 206018 steps / epoch
