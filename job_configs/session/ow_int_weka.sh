185ad1443a5c1bdb71a1cf53cda1e2d1b57c8d59
huongngo-8 <hvn2002@uw.edu>
2024-12-24 18:02:36 -0800

    new interactive job config

--- job_configs/session/ow_int_weka.sh
+++
@@ -1,8 +0,0 @@
-beaker session create \
---budget ai2/oe-data \
---bare \
---image beaker://huongn/data_processing \
---mount weka://oe-data-default=/weka \
---name ow_int_sesh \
---priority normal \
---workspace ai2/open-whisper
