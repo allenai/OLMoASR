0c04f9e6c1ca55ef12866866b3d3e81b09ce69fc
huongngo-8 <hvn2002@uw.edu>
2025-01-04 19:48:04 -0500

    adding multilingual model config

--- open_whisper/config/model_dims.py
+++ open_whisper/config/model_dims.py
@@ -86,4 +86,16 @@ VARIANT_TO_DIMS = {
         n_text_head=20,
         n_text_layer=32,
     ),
+    "tiny_ml": ModelDimensions(
+        n_mels=80,
+        n_audio_ctx=1500,
+        n_audio_state=384,
+        n_audio_head=6,
+        n_audio_layer=4,
+        n_vocab=51865,
+        n_text_ctx=448,
+        n_text_state=384,
+        n_text_head=6,
+        n_text_layer=4,
+    ),
 }
