e952ff3726d57282f8b4a6c00d610870c7cac793
huongngo-8 <hvn2002@uw.edu>
2025-02-04 01:35:06 -0800

    remove thread to wait for run_async_eval completion

--- scripts/training/prod/train.py
+++ scripts/training/prod/train.py
@@ -13,7 +13,6 @@ from itertools import chain
 from collections import defaultdict
 import gzip
 import subprocess
-import threading
 
 import torch
 import torch.nn.functional as F
@@ -1502,15 +1501,7 @@ def run_async_eval(
     print(f"{cmd=}")
 
     if rank == 0:
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setpgrp)
-
-        # Define a function to wait for the process to finish
-        def wait_for_completion():
-            process.wait()
-        
-        # Start a background thread to monitor the process
-        thread = threading.Thread(target=wait_for_completion, daemon=True)
-        thread.start()
+        subprocess.Popen(cmd)
 
 
 def cleanup():
