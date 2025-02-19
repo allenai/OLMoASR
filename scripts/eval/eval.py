73bed1dbc84f9c3df3f229cfe38e78ba677d1d7f
huongngo-8 <hvn2002@uw.edu>
2025-02-19 00:47:52 -0800

    temporarily refactoring how to get audio file path for coraal

--- scripts/eval/eval.py
+++ scripts/eval/eval.py
@@ -175,7 +175,7 @@ class CORAAL:
             segment_basename, basefile, _, _, _, _, _, content, *_ = segment
             sub_folder = os.path.join(self.root_dir, basefile.split("_")[0].lower())
             # audio_file = os.path.join(sub_folder, "segments", segment_basename)
-            audio_file = os.path.join(sub_folder, segment_basename)
+            audio_file = os.path.join(sub_folder, segment_basename.replace(".wav", ".mp3"))
             audio_files.append(audio_file)
             transcript_texts.append(content)
 
