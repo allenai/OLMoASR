f0a992621b848fdf1e4d5b6603dc36b1203bc91a
huongngo-8 <hvn2002@uw.edu>
2025-03-19 11:47:49 -0700

    refactor to normalize text, tokenization of data for truncation in doc tagging mode and generalize var. names so script can work for seg or doc level tagging

--- scripts/data/filtering/tag_sim_score.py
+++ scripts/data/filtering/tag_sim_score.py
@@ -1,4 +1,5 @@
-from vllm import LLM
+from vllm import LLM, SamplingParams
+from vllm.inputs import TokensPrompt
 import os
 from tqdm import tqdm
 import torch
@@ -7,13 +8,18 @@ from time import time
 import gzip
 import glob
 import multiprocessing
+from functools import partial
 from torch.utils.data import Dataset, DataLoader
 from typing import List, Dict, Literal
 from fire import Fire
 import torch.nn.functional as F
 import numpy as np
 import webvtt
+import sys
+
+sys.path.append("/stage")
 from open_whisper.utils import TranscriptReader
+from whisper.normalizers import EnglishTextNormalizer
 
 
 def normalize(x, p=2, axis=1, eps=1e-12):
@@ -34,33 +40,68 @@ def open_file(file_path) -> List[Dict]:
     return file_path, data
 
 
+def init_tokenizer(worker_id: int, llm):
+    global tokenizer
+    tokenizer = llm.llm_engine.get_tokenizer_group()
+
+
+def collate_fn(batch):
+    encoded_man, encoded_mach = zip(*batch)
+    encoded_man = [TokensPrompt(prompt_token_ids=item) for item in encoded_man]
+    encoded_mach = [TokensPrompt(prompt_token_ids=item) for item in encoded_mach]
+
+    return encoded_man, encoded_mach
+
+
 class SamplesDictsDataset(Dataset):
     def __init__(self, data, level):
         self.data = data
         self.level = level
+        self.normalizer = EnglishTextNormalizer()
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        man_seg_text = (
+        man_text = (
             self.data[idx]["seg_text"]
             if self.level == "seg"
             else self.get_man_text(self.data[idx]["content"])
         )
-        mach_seg_text = (
+        mach_text = (
             self.data[idx]["mach_seg_text"]
             if self.level == "seg"
             else self.get_mach_text(self.data[idx]["mach_content"])
         )
 
-        if man_seg_text == "":
-            man_seg_text = " "
+        try:
+            norm_man_text = self.normalizer(man_text)
+        except Exception:
+            norm_man_text = man_text
 
-        if mach_seg_text == "":
-            mach_seg_text = " "
+        try:
+            norm_mach_text = self.normalizer(mach_text)
+        except Exception:
+            norm_mach_text = mach_text
 
-        return man_seg_text, mach_seg_text
+        if self.level == "doc":
+            encoded_man = tokenizer.encode(norm_man_text)
+            encoded_mach = tokenizer.encode(norm_mach_text)
+
+            if len(encoded_man) >= 32768:
+                encoded_man = encoded_man[:32768]
+
+            if len(encoded_mach) >= 32768:
+                encoded_mach = encoded_mach[:32768]
+            return encoded_man, encoded_mach
+        else:
+            if norm_man_text == "":
+                norm_man_text = " "
+
+            if norm_mach_text == "":
+                norm_mach_text = " "
+
+            return norm_man_text, norm_mach_text
 
     def get_man_text(self, man_content):
         reader = TranscriptReader(
@@ -70,56 +111,68 @@ class SamplesDictsDataset(Dataset):
         )
         t_dict, *_ = reader.read()
         man_text = reader.extract_text(t_dict)
-        return man_text
+        return man_text.strip()
 
     def get_mach_text(self, mach_content):
-        content = webvtt.from_string(mach_content)
-        modified_content = []
-        if len(content) > 0:
-            if len(content) > 1:
-                if content[0].text == content[1].text:
-                    modified_content.append(content[0])
-                    start = 2
-                else:
+        mach_text = ""
+        if mach_content != "":
+            content = webvtt.from_string(mach_content)
+            modified_content = []
+            if len(content) > 0:
+                if len(content) > 1:
+                    if content[0].text == content[1].text:
+                        modified_content.append(content[0])
+                        start = 2
+                    else:
+                        start = 0
+                elif len(content) == 1:
                     start = 0
-            elif len(content) == 1:
-                start = 0
 
-            for i in range(start, len(content)):
-                caption = content[i]
-                if "\n" not in caption.text:
-                    modified_content.append(caption)
-                elif "\n" in caption.text and i == len(content) - 1:
-                    caption.text = caption.text.split("\n")[-1]
-                    modified_content.append(caption)
+                for i in range(start, len(content)):
+                    caption = content[i]
+                    if "\n" not in caption.text:
+                        modified_content.append(caption)
+                    elif "\n" in caption.text and i == len(content) - 1:
+                        caption.text = caption.text.split("\n")[-1]
+                        modified_content.append(caption)
 
-            mach_text = " ".join([caption.text for caption in modified_content])
-        else:
-            mach_text = ""
-        return mach_text
+                mach_text = " ".join([caption.text for caption in modified_content])
+
+        return mach_text.strip()
 
 
 def process_jsonl(llm, data, batch_size, num_workers, level):
+    if level == "doc":
+        partial_init_tokenizer = partial(init_tokenizer, llm=llm)
+
     dataset = SamplesDictsDataset(data, level)
 
-    dataloader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        drop_last=False,
-        num_workers=num_workers,
-    )
+    if level == "doc":
+        dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=num_workers,
+            worker_init_fn=partial_init_tokenizer,
+            collate_fn=collate_fn,
+        )
+    else:
+        dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=num_workers,
+        )
 
     scores = []
     start = time()
     for _, batch in enumerate(tqdm(dataloader, total=len(dataloader))):
-        man_seg_text, mach_seg_text = batch
-        print(f"{len(man_seg_text)=}, {len(mach_seg_text)=}")
-        print(f"{man_seg_text[0]=}")
-        print(f"{mach_seg_text[0]=}")
+        man_input, mach_input = batch
 
-        man_output = llm.embed(man_seg_text)
-        mach_output = llm.embed(mach_seg_text)
+        man_output = llm.embed(man_input)
+        mach_output = llm.embed(mach_input)
 
         man_embeds = F.normalize(
             torch.stack(
@@ -137,10 +190,6 @@ def process_jsonl(llm, data, batch_size, num_workers, level):
             dim=-1,
         )
 
-        print(f"{man_embeds.shape=}, {mach_embeds.shape=}")
-        print(f"{man_embeds[0]=}")
-        print(f"{mach_embeds[0]=}")
-
         batch_score = F.cosine_similarity(man_embeds, mach_embeds, dim=-1)
         print(f"{batch_score.shape=}")
         batch_score = batch_score.tolist()
@@ -152,8 +201,11 @@ def process_jsonl(llm, data, batch_size, num_workers, level):
     print(f"Number of segments: {len(data)}")
     assert len(scores) == len(data)
 
-    for i, segment in enumerate(data):
-        segment["seg_man_mach_score"] = scores[i]
+    for i, sample in enumerate(data):
+        if level == "doc":
+            sample["sim_score"] = scores[i]
+        else:
+            sample["seg_sim_score"] = scores[i]
 
     return data
 
@@ -161,22 +213,22 @@ def process_jsonl(llm, data, batch_size, num_workers, level):
 def main(
     source_dir,
     output_dir,
-    # start_shard_idx: int,
-    # job_batch_size: int,
+    start_shard_idx: int,
+    job_batch_size: int,
     batch_size: int,
     num_workers: int,
-    level: Literal["seg", "doc"],
+    level: str,
 ):
     os.makedirs(output_dir, exist_ok=True)
-    # job_idx = int(os.getenv("BEAKER_REPLICA_RANK"))
-    # job_start_shard_idx = start_shard_idx + (job_idx * job_batch_size)
-    # job_end_shard_idx = start_shard_idx + ((job_idx + 1) * job_batch_size)
-    # print(f"{job_start_shard_idx=}")
-    # print(f"{job_end_shard_idx=}")
-    # data_shard_paths = sorted(glob.glob(source_dir + "/*.jsonl.gz"))[
-    #     job_start_shard_idx : job_end_shard_idx
-    #     + 1  # forgot to add 1 here! so last shard in output file is excluded
-    # ]
+    job_idx = int(os.getenv("BEAKER_REPLICA_RANK"))
+    job_start_shard_idx = start_shard_idx + (job_idx * job_batch_size)
+    job_end_shard_idx = start_shard_idx + ((job_idx + 1) * job_batch_size)
+    print(f"{job_start_shard_idx=}")
+    print(f"{job_end_shard_idx=}")
+    data_shard_paths = sorted(glob.glob(source_dir + "/*.jsonl.gz"))[
+        job_start_shard_idx : job_end_shard_idx
+        + 1  # forgot to add 1 here! so last shard in output file is excluded
+    ]
     data_shard_paths = sorted(glob.glob(source_dir + "/*.jsonl.gz"))
     print(f"{len(data_shard_paths)=}")
     print(f"{data_shard_paths[:5]=}")
@@ -191,7 +243,7 @@ def main(
         )
 
     shard_paths, all_data = zip(*result)
-    print(f"All segments in job: {sum(len(data) for data in all_data)}")
+    print(f"All samples in job: {sum(len(data) for data in all_data)}")
 
     llm = LLM(
         model="Alibaba-NLP/gte-Qwen2-1.5B-instruct",
@@ -212,4 +264,10 @@ def main(
 
 if __name__ == "__main__":
     Fire(main)
-    # main(source_dir="temp", output_dir="temp_2", batch_size=8192, num_workers=28)
+    # main(
+    #     source_dir="temp_doc",
+    #     output_dir="temp_doc_2",
+    #     batch_size=950,
+    #     num_workers=28,
+    #     level="doc",
+    # )
