7c6ad8c9cc8405d22173edddccdc25d7d0cdba5c
huongngo-8 <hvn2002@uw.edu>
2025-04-28 19:11:37 -0700

    temporarily refactoring transcribe import function in model script (inf_model.py_

--- open_whisper/inf_model.py
+++ open_whisper/inf_model.py
@@ -8,7 +8,8 @@ from open_whisper.config.model_dims import ModelDimensions
 
 from whisper.decoding import decode as decode_function
 from whisper.decoding import detect_language as detect_language_function
-from whisper.transcribe import transcribe as transcribe_function
+# from whisper.transcribe import transcribe as transcribe_function
+from open_whisper.transcribe import transcribe as transcribe_function
 
 
 class LayerNorm(nn.LayerNorm):
