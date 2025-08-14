48b91839ac0211a2105634c55dfc731265eb026d
huongngo-8 <hvn2002@uw.edu>
2025-08-14 08:10:29 +0000

    new demo code

--- demo/demo.py
+++ demo/demo.py
@@ -65,10 +65,12 @@ def hf_chunk_transcribe(audio_file, timestamp_text, transcription_text):
     print(f"{result['text']=}\n")
     print(f"{result['chunks']=}\n")
 
-    text = result["text"].strip().replace("\n", " ")
-    text = re.sub(r"(foreign|foreign you}you)\s*$", "", text)
+    # text = result["text"].strip().replace("\n", " ")
+    # text = re.sub(r"(foreign|foreign you|you)\s*$", "", text)
 
-    chunks = hf_process_chunks(result["chunks"])
+    chunks, text = hf_process_chunks(result["chunks"])
+    print(f"{chunks=}\n")
+    print(f"{text=}\n")
 
     # Edit components
     transSoup = BeautifulSoup(transcription_text, "html.parser")
@@ -105,11 +107,13 @@ def olmoasr_seq_transcribe(audio_file, timestamp_text, transcription_text):
     print(f"{result['text']=}\n")
     print(f"{result['segments']=}\n")
 
-    text = result["text"].strip().replace("\n", " ")
-    text = re.sub(r"(foreign|foreign you|Thank you for watching!|. you)\s*$", "", text)
+    # text = result["text"].strip().replace("\n", " ")
+    # text = re.sub(r"(foreign|foreign you|Thank you for watching!|. you)\s*$", "", text)
 
-    chunks = olmoasr_process_chunks(result["segments"])
-
+    chunks, text = olmoasr_process_chunks(result["segments"])
+    print(f"{chunks=}\n")
+    print(f"{text=}\n")
+    
     # Edit components
     transSoup = BeautifulSoup(transcription_text, "html.parser")
     transText = transSoup.find(id="transcriptionText")
@@ -150,10 +154,12 @@ def hf_seq_transcribe(audio_file, timestamp_text, transcription_text):
     print(f"{result['text']=}\n")
     print(f"{result['chunks']=}\n")
 
-    text = result["text"].strip().replace("\n", " ")
-    text = re.sub(r"(foreign|foreign you|you)\s*$", "", text)
+    # text = result["text"].strip().replace("\n", " ")
+    # text = re.sub(r"(foreign|foreign you|you)\s*$", "", text)
 
-    chunks = hf_seq_process_chunks(result["chunks"])
+    chunks, text = hf_seq_process_chunks(result["chunks"])
+    print(f"{text=}\n")
+    print(f"{chunks=}\n")
 
     # Edit components
     transSoup = BeautifulSoup(transcription_text, "html.parser")
@@ -182,42 +188,61 @@ def main_transcribe(inference_strategy, audio_file, timestamp_text, transcriptio
 
 def olmoasr_process_chunks(chunks):
     processed_chunks = []
+    processed_chunks_text = []
     for chunk in chunks:
         text = chunk["text"].strip()
         if not re.match(
-            r"(foreign|foreign you|Thank you for watching!|you|you there)\s*$", text
+            r"\s*(foreign you|foreign|Thank you for watching!|you there|you)\s*$", text
         ):
             if text.strip() == "":
                 continue
             start = chunk["start"]
             end = chunk["end"]
             pattern = r"\n(?!\d+\.\d+\s*-->)"
-            text = re.sub(pattern, " ", text)
+            text = re.sub(pattern, "", text)
+            processed_chunks_text.append(text.strip())
             processed_chunks.append(f"{start:.2f} --> {end:.2f}: {text} <br>")
         else:
             break
-    return "\n".join(processed_chunks)
+    print(f"{processed_chunks=}\n")
+    print(f"{processed_chunks_text=}\n")
+    print(re.search(r"\s*foreign\s*$", processed_chunks_text[-1]) if processed_chunks_text else None)
+
+    if processed_chunks_text and re.search(r"\s*foreign\s*$", processed_chunks_text[-1]):
+        processed_chunks_text[-1] = re.sub(r"\s*foreign\s*$", "", processed_chunks_text[-1])
+        processed_chunks[-1] = re.sub(r"foreign\s*<br>", "<br>", processed_chunks[-1])
+    return "\n".join(processed_chunks), " ".join(processed_chunks_text)
 
 
 def hf_process_chunks(chunks):
     processed_chunks = []
+    processed_chunks_text = []
     for chunk in chunks:
         text = chunk["text"].strip()
-        if not re.match(r"(foreign|foreign you|you|you there)\s*$", text):
+        if not re.match(r"(foreign you|foreign|you there|you)\s*$", text):
             if text.strip() == "":
                 continue
             start = chunk["timestamp"][0]
             end = chunk["timestamp"][1]
             pattern = r"\n(?!\d+\.\d+\s*-->)"
-            text = re.sub(pattern, " ", text)
+            text = re.sub(pattern, "", text)
+            processed_chunks_text.append(text.strip())
             processed_chunks.append(f"{start:.2f} --> {end:.2f}: {text.strip()} <br>")
         else:
             break
-    return "\n".join(processed_chunks)
+    print(f"{processed_chunks=}\n")
+    print(f"{processed_chunks_text=}\n")
+    print(re.search(r"\s*foreign\s*$", processed_chunks_text[-1]) if processed_chunks_text else None)
+
+    if processed_chunks_text and re.search(r"\s*foreign\s*$", processed_chunks_text[-1]):
+        processed_chunks_text[-1] = re.sub(r"\s*foreign\s*$", "", processed_chunks_text[-1])
+        processed_chunks[-1] = re.sub(r"foreign\s*<br>", "<br>", processed_chunks[-1])
+    return "\n".join(processed_chunks), " ".join(processed_chunks_text)
 
 
 def hf_seq_process_chunks(chunks):
     processed_chunks = []
+    processed_chunks_text = []
     delta_time = 0.0
     global_start = chunks[0]["timestamp"][0]
     prev_end = -1.0
@@ -225,7 +250,7 @@ def hf_seq_process_chunks(chunks):
     accumulate_ts = False
     for chunk in chunks:
         text = chunk["text"].strip()
-        if not re.match(r"(foreign|foreign you|you|you there)\s*$", text):
+        if not re.match(r"\s*(foreign you|foreign|you there|you)\s*$", text):
             if text.strip() == "":
                 continue
             start = chunk["timestamp"][0]
@@ -248,11 +273,19 @@ def hf_seq_process_chunks(chunks):
             # print(f"{start=}, {end=}, {prev_dur=}")
 
             pattern = r"\n(?!\d+\.\d+\s*-->)"
-            text = re.sub(pattern, " ", text)
+            text = re.sub(pattern, "", text)
+            processed_chunks_text.append(text.strip())
             processed_chunks.append(f"{start:.2f} --> {end:.2f}: {text.strip()} <br>")
         else:
             break
-    return "\n".join(processed_chunks)
+    print(f"{processed_chunks=}\n")
+    print(f"{processed_chunks_text=}\n")
+    print(re.search(r"\s*foreign\s*$", processed_chunks_text[-1]) if processed_chunks_text else None)
+
+    if processed_chunks_text and re.search(r"\s*foreign\s*$", processed_chunks_text[-1]):
+        processed_chunks_text[-1] = re.sub(r"\s*foreign\s*$", "", processed_chunks_text[-1])
+        processed_chunks[-1] = re.sub(r"foreign\s*<br>", "<br>", processed_chunks[-1])
+    return "\n".join(processed_chunks), " ".join(processed_chunks_text)
 
 
 original_timestamp_html = """
@@ -264,7 +297,7 @@ original_timestamp_html = """
 
 original_transcription_html = """
     <div style="background: white; border: 1px solid #d1d5db; border-radius: 8px; padding: 16px; width: 100%; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1); flex: 1; margin-right: 10px;">
-        <div style="color: #374151; font-size: 14px; font-weight: 500; margin-bottom: 8px;">Timestamp Text</div>
+        <div style="color: #374151; font-size: 14px; font-weight: 500; margin-bottom: 8px;">Transcription Text</div>
         <div id="transcriptionText"; style="color: #6b7280; font-size: 14px; line-height: 1.5; min-height: 100px; font-family: system-ui, sans-serif;"></div>
     </div>
     """
